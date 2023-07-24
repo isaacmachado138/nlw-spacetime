c0927c82183a3c5c8da6fa7bf69ccfb5c51b78d1
isaac.machado <isaac.machado@35.168.70.227>
2023-07-24 10:21:50 -0300

    Web project

---
+++ web/src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+
+  if (!token) {
+    return NextResponse.redirect(signInURL, {
+      headers: {
+        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
+      },
+    })
+  }
+
+  return NextResponse.next()
+}
+
+// para qual rota serao chamadas as middlewares, nesse caso, para qualquer que comece com memories
+export const config = {
+  matcher: '/memories/:path*',
+}
